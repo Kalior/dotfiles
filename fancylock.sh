404c789c159075a994c4ecf6d344d579e1f3f3b5
Kalior <kalioragus@gmail.com>
2016-03-10 19:28:08 +0100

    Update to fancylock after update to i3lock

--- fancylock.sh
+++ fancylock.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
- 
+
 # https://github.com/meskarune/i3lock-fancy/
 
 # Dependencies:
 # imagemagick
 # i3lock-color-git
 # scrot
- 
+
 IMAGE=/tmp/i3lock.png
- 
+
 # All options are here: http://www.imagemagick.org/Usage/blur/#blur_args
 #BLURTYPE="0x5" # 7.52s
 #BLURTYPE="0x2" # 4.39s
@@ -19,5 +19,5 @@ BLURTYPE="4x6"
 
 scrot $IMAGE
 convert $IMAGE -level 0%,100%,0.6 -blur $BLURTYPE $IMAGE
-i3lock --textcolor=ffffff00 --insidecolor=ffffff1c --ringcolor=ffffff3e --linecolor=ffffff00 --keyhlcolor=00000080 --ringvercolor=00000000 --insidevercolor=0000001c --ringwrongcolor=00000055 --insidewrongcolor=0000001c  -i $IMAGE
+i3lock-color --textcolor=ffffff00 --insidecolor=ffffff1c --ringcolor=ffffff3e --linecolor=ffffff00 --keyhlcolor=00000080 --ringvercolor=00000000 --insidevercolor=0000001c --ringwrongcolor=00000055 --insidewrongcolor=0000001c  -i $IMAGE
 rm $IMAGE
