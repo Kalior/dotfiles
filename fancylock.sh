0dab578e4e3df840563f53c0fdb52f9b1a513f07
Kalior <kalioragus@gmail.com>
2016-06-01 16:59:18 +0200

    Update fancylock

--- fancylock.sh
+++ fancylock.sh
@@ -19,5 +19,5 @@ BLURTYPE="4x6"
 
 scrot $IMAGE
 convert $IMAGE -level 0%,100%,0.6 -blur $BLURTYPE $IMAGE
-i3lock-color --textcolor=ffffff00 --insidecolor=ffffff1c --ringcolor=ffffff3e --linecolor=ffffff00 --keyhlcolor=00000080 --ringvercolor=00000000 --insidevercolor=0000001c --ringwrongcolor=00000055 --insidewrongcolor=0000001c  -i $IMAGE
+i3lock --textcolor=ffffff00 --insidecolor=ffffff1c --ringcolor=ffffff3e --linecolor=ffffff00 --keyhlcolor=00000080 --ringvercolor=00000000 --insidevercolor=0000001c --ringwrongcolor=00000055 --insidewrongcolor=0000001c  -i $IMAGE
 rm $IMAGE
